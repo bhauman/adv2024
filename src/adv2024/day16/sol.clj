424db517587ea58ef06a221675dece699f079533
Bruce Hauman <bhauman@gmail.com>
2024-12-16 21:21:22 -0600

    reimplimented day 16 with djikstra's

--- src/adv2024/day16/sol.clj
+++ src/adv2024/day16/sol.clj
@@ -1,7 +1,8 @@
 (ns adv2024.day16.sol
   (:require
    [clojure.string :as string]
-   [clojure.math.combinatorics :as combo]))
+   [clojure.math.combinatorics :as combo]
+   [clojure.data.priority-map :refer [priority-map priority-map-keyfn]]))
 
 (def input (->> (slurp "src/adv2024/day16/input.txt")
                 string/split-lines
@@ -20,67 +21,64 @@
        (map vec)
        (into #{})))
 
-(defn next-steps [m p dir path-set]
+(defn next-nodes [m [p dir]]
   (->> [dir (turn-right dir) (turn-left dir)]
        (map #(vector (mapv + p %) %))
-       (filter (comp m first))
-       (remove (comp path-set first))))
+       (filter (comp m first))))
 
-(defn extend-path [m [sc [p dir] path-set]]
-  (let [steps (next-steps m p dir path-set)]
-    (map
-     (fn [[np ndir]]
-       [(+ sc (if (= ndir dir) 1 1001))
-        [np ndir]
-        (conj path-set np)])
-     steps)))
-
-(defn extend-paths [m path-maps]
-  (let [paths (mapcat (partial extend-path m) (vals path-maps))
-        num-paths (count paths)
-        limit (int (* num-paths 0.75))]
-    (->> paths
-         (sort-by first)
-         ;; tuned this for faster iterations on part 2
-         (take (if (> limit 700) limit num-paths)) 
-         (reduce
-          (fn [accum [sc head path-set :as new-entry]]
-            (update accum head
-                    (fn [[old-sc _ old-path-set :as orig-entry]]
-                      (cond
-                        (nil? orig-entry) new-entry
-                        (= sc old-sc) [sc head (into old-path-set path-set)]
-                        (< sc old-sc) new-entry
-                        :else orig-entry))))
-          {}))))
+(defn step [m [unvisited visited]]
+  (let [[[_ dir :as p-dir] sc] (first unvisited)
+        neighs (->> (next-nodes m p-dir) (remove visited))]
+    [(-> (reduce
+          (fn [unv [np ndir :as neig]]
+            (update unv neig #(min (+ sc (if (= ndir dir) 1 1001))
+                                   (or % Integer/MAX_VALUE))))
+          unvisited
+          neighs)
+         (dissoc p-dir))
+     (assoc visited p-dir sc)]))
 
 ;; part 1
-#_(def best-score
-    (time
-     (->> (iterate (partial extend-paths maze)
-                   {[start [0 1]] [0 [start [0 1]] #{start}]})
-          (take-while not-empty)
-          (mapcat #(select-keys % [[end [0 1]] [end [-1 0]]]))
-          (take 6)
-          (map val)
-          (sort-by first)
-          ffirst))) ; => 105508
+#_(time (->> [(priority-map [start [0 1]] 0) {}]
+             (iterate (partial step maze))
+             (take-while (comp not-empty first))
+             last
+             (#(select-keys (second %) [[end [0 1]] [end [-1 0]]]))
+             (map val)
+             sort
+             first))
 
+
+;; modify part 1 to collect the paths
+(defn step2 [m [unvisited visited]]
+  (let [[[_ dir :as p-dir] [sc path-set]] (first unvisited)
+        neighs (->> (next-nodes m p-dir) (remove visited))]
+    [(-> (reduce
+          (fn [unv [np ndir :as neig]]
+            (update unv neig
+                    (fn [[old-sc old-path-set]]
+                      (let [nsc (+ sc (if (= ndir dir) 1 1001))
+                            n-path-set (conj path-set np)]
+                        (cond
+                          (nil? old-sc)  [nsc n-path-set]
+                          (< nsc old-sc) [nsc n-path-set]                          
+                          (= old-sc nsc) [nsc (into old-path-set n-path-set)]
+                          :else [old-sc old-path-set])))))
+          unvisited
+          neighs)
+         (dissoc p-dir))
+     (assoc visited p-dir (val (first unvisited)))]))
 
 ;; part 2
-#_(def num-best-seats
-    (time
-     (->> (iterate (partial extend-paths maze)
-                   {[start [0 1]] [0 [start [0 1]] #{start}]})
-          (take-while not-empty)
-          (mapcat #(select-keys % [[end [0 1]] [end [-1 0]]]))
-          (take 6)
-          (map val)
-          (sort-by first)
-          first
-          last
-          count))) ; => 548
-
-
-
+#_(->> [(priority-map-keyfn first [start [0 1]] [0 #{start}])
+        {}]
+       (iterate (partial step2 maze))
+       (take-while (comp not-empty first))
+       last
+       (#(select-keys (second %) [[end [0 1]] [end [-1 0]]]))
+       (map val)
+       (sort-by first)
+       first
+       second
+       count)
 
